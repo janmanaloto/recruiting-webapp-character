2c51b47b92ab361abd3b491e7d25e085d08456d9
Jan Manaloto <janmanaloto@gmail.com>
2024-09-16 02:27:58 -0400

    3 - When clicking on a class, display the minimum required statistics for that class

--- src/modules/class/components/Class.js
+++ src/modules/class/components/Class.js
@@ -1,14 +1,32 @@
 import { CLASS_LIST } from "../../../consts";
+import { useState } from "react";
+import { Attribute } from "../../attribute/components/Attribute";
 
 export function Class({ className, attributes }) {
+  const [isActive, setIsActive] = useState(false);
   const data = CLASS_LIST[className];
   const isMeetMinimums = Object.keys(data).every((attribute) => {
     const value = attributes[attribute];
     return value >= data[attribute];
   });
-  return isMeetMinimums ? (
-    <h2 style={{ color: "green" }}>{className}</h2>
-  ) : (
-    <h2 style={{ color: "red" }}>{className}</h2>
+
+  return (
+    <>
+      <h2
+        style={{ color: isMeetMinimums ? "green" : "#61DAFB" }}
+        onClick={() => setIsActive(!isActive)}
+      >
+        {className}
+      </h2>
+      {isActive && (
+        <div>
+          {Object.keys(data).map((attribute) => (
+            <h4>
+              {attribute}: {data[attribute]}
+            </h4>
+          ))}
+        </div>
+      )}
+    </>
   );
 }
