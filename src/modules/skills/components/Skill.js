652c33f36edc3af68d89d70995423df4619e5c20
Jan Manaloto <janmanaloto@gmail.com>
2024-09-16 02:55:45 -0400

    5-1 - Display skill attribute modifier

--- src/modules/skills/components/Skill.js
+++ src/modules/skills/components/Skill.js
@@ -1,3 +1,11 @@
+import { useAppContext } from "../../app/state/app.context";
+
 export default function Skill({ skill }) {
-  return <div>{skill.name}</div>;
+  // const { attributes } = useAppContext();
+  const { attributeModifier } = skill;
+  return (
+    <div>
+      {skill.name}: (Modifier: {attributeModifier})
+    </div>
+  );
 }
